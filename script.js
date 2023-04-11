f6cb9bb36f492d3b48b21995b62cace11911975e
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-11 15:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
+   
 function hasKey(key) {
-  //   write your code here
+	//   write your code here
+	for(let currKey : sampleObject)
+		{
+			if(currKey==key)
+				{
+					return true;
+				}
+		}
+		return false;
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
+ 
